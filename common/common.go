e784e6fb8e4c4ba5e57cceabd691a1da8406cc51
univana <univana@outlook.com>
2021-04-23 17:20:13 +0800

    修改用户角色的相关设置

--- common/common.go
+++ common/common.go
@@ -5,31 +5,24 @@ import "github.com/astaxie/beego"
 // session
 const SessionName = "__tiny-recruit_session__"
 
-//正则表达式
+/*//正则表达式
 const RegexpEmail = `^(\w)+(\.\w+)*@(\w)+((\.\w+)+)$`
 
 // 默认PageSize
 const PageSize = 20
-const RollPage = 4
+const RollPage = 4*/
 
 const WorkingDirectory = "./"
 
-// 用户权限
-const (
-	//管理员.
-	MemberAdminRole = 0
-	//普通用户.
-	MemberGeneralRole = 1
-)
-
+//Role 返回用户权限名
 func Role(role int) string {
 
-	if role == MemberAdminRole {
+	if role == 0 {
 		return "管理员"
-	} else if role == MemberGeneralRole {
-		return "普通用户"
+	} else if role == 1 {
+		return "求职者"
 	} else {
-		return ""
+		return "企业用户"
 	}
 }
 
@@ -43,8 +36,9 @@ func DefaultAvatar() string {
 	return beego.AppConfig.DefaultString("avatar", "/static/images/avatar.jpg")
 }
 
-//TODO :fix
+/*//TODO :fix
 //默认封面
 func DefaultCover() string {
 	return beego.AppConfig.DefaultString("cover", "/static/images/book.png")
 }
+*/
